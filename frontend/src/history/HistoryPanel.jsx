6c6cfa56dce58d18882ec32eb1042ca2e82a783b
Vish <155484823+vish2285@users.noreply.github.com>
2025-08-19 18:55:15 +0000

    Getting all  MCQ histroy

--- frontend/src/history/HistoryPanel.jsx
+++ frontend/src/history/HistoryPanel.jsx
@@ -1,5 +1,50 @@
 import "react"
+import { useState, useEffect } from "react"
+import { MCQChallenge } from "../challenge/MCQChallenge"
 
 export function HistoryPanel(){
-    return<></>
+    
+    const [history, setHistory] = useState([])
+    const [isLoading, setIsLoading] = useState(null)
+    const [error, setError ] = useState(null)
+
+
+    useEffect(()=>{
+        fetchHistory()
+    },[])
+
+    const fetchHistory = async() =>{ 
+        isLoading(false)
+    }
+
+    if (isLoading){
+        <div>
+            Loading...
+        </div>
+    }
+
+    if (error) {
+        return <div className="error-message">
+            <p>{error}</p>
+            <button onClick={fetchHistory}>Retry</button>
+        </div>
+    }
+
+    
+    return<div className="history-panel">
+            <h2>History</h2>
+            {length.history === 0 ? <p>No History</p> : 
+            <div className="history-list">
+                {history.map((challenge)=>{
+                    return <MCQChallenge 
+                    challenge={challenge}
+                    key={challenge.id}
+                    showExplanation={true}
+                    />
+                })}
+            </div>
+            
+            }
+
+    </div>
 }
