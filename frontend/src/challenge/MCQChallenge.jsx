4fa44b8223c484d796d4debbacb88204022887de
Vish <155484823+vish2285@users.noreply.github.com>
2025-08-19 18:38:10 +0000

    MCQChallenge DONE

--- frontend/src/challenge/MCQChallenge.jsx
+++ frontend/src/challenge/MCQChallenge.jsx
@@ -1,33 +1,52 @@
 import "react"
-import {useState} from 'react'
+import {useState} from "react"
 
-export function MCQChallenge(challenge, showExplanation= true){
-    const [selectedOption, setSelectedOption] = useState(null) 
+export function MCQChallenge({challenge, showExplanation = false}) {
+    const [selectedOption, setSelectedOption] = useState(null)
     const [shouldShowExplanation, setShouldShowExplanation] = useState(showExplanation)
 
-    const options = typeof challenge.options === 'string'
+    const options = typeof challenge.options === "string"
         ? JSON.parse(challenge.options)
         : challenge.options
-    
-    const handleOptionSelect = (index) =>{
-        if (selectedOption !== null) 
-        return; setSelectedOption(index)
+
+    const handleOptionSelect = (index) => {
+        if (selectedOption !== null) return;
+        setSelectedOption(index)
         setShouldShowExplanation(true)
     }
-    
+
     const getOptionClass = (index) => {
         if (selectedOption === null) return "option"
-        
-        if (index === challenge.correct_answer_id){
+
+        if (index === challenge.correct_answer_id) {
             return "option correct"
         }
-        
-        if (index === selectedOption && selectedOption !== challenge.correct_answer_id){
+        if (selectedOption === index && index !== challenge.correct_answer_id) {
             return "option incorrect"
         }
-        
+
         return "option"
     }
-    
-    return<></>
+
+    return <div className="challenge-display">
+        <p><strong>Difficulty</strong>: {challenge.difficulty}</p>
+        <p className="challenge-title">{challenge.title}</p>
+        <div className="options">
+            {options.map((option, index) => (
+                <div
+                    className={getOptionClass(index)}
+                    key={index}
+                    onClick={() => handleOptionSelect(index)}
+                >
+                    {option}
+                </div>
+            ))}
+        </div>
+        {shouldShowExplanation && selectedOption !== null && (
+            <div className="explanation">
+                <h4>Explanation</h4>
+                <p>{challenge.explanation}</p>
+            </div>
+        )}
+    </div>
 }
